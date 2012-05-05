35e56eda042894c5fff75a052043e4a9eb30aa3c
Ubuntu <ubuntu@ip-10-202-81-59.ec2.internal>
2012-05-05 17:31:33 +0000

    Hmm, dont know how this worked last night but oh well. It is working now.

--- cron/upload_to_s3.py
+++ cron/upload_to_s3.py
@@ -2,13 +2,15 @@
 
 import boto
 import glob
+import os
+import os.path
 
 c = boto.connect_s3("AKIAIJKXBJLHXT7HIPTQ","Kp86pC6CB2f0syp+Uyix7T5jjqjb4oMmMSefpT13")
-b = c.create_bucket('karmafeeder')
+b = c.get_bucket('karmafeeder',validate=False)
 from boto.s3.key import Key
 k = Key(b)
 
-for s in glob.glob("securities_db/*/*/*/*/*/*"):
-    k.key = s
+for s in glob.glob(os.path.join(os.path.dirname(__file__),"securities_db/*/*/*/*/*/*")):
+    k.key = '/'.join(s.split('/')[-7:])
     k.set_contents_from_filename(s)
     os.remove(s)
